48c8a1dd2674766de1e388a394c6489450ca4e86
SichangiFelix <sichangifelix23@gmail.com>
2023-03-16 18:01:32 +0300

    Starting afresh

--- artllery_backend/artllery/authservice/serializers.py
+++
@@ -1,8 +0,0 @@
-# from rest_framework import serializers
-# from .models import *
-# from djoser.serializers import UserCreateSerializer, UserSerializer
-
-# class UserCreateSerializer(UserCreateSerializer):
-#     class Meta(UserCreateSerializer):
-#         model = User
-#         fields = ('id', 'email', 'username', 'password', 'first_name', 'last_name', 'phone')
