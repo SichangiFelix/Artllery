e51e69b05eb6a274c5427e57d0c33b61bd3893cd
SichangiFelix <sichangifelix23@gmail.com>
2023-04-02 00:30:34 +0300

    Moved authservice provider to root of the app

--- artllery_mobile/lib/main.dart
+++ artllery_mobile/lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:artllery_mobile/features/authentication/screens/signin_screen.dart';
+import 'package:artllery_mobile/features/authentication/services/auth_service.dart';
 import 'package:flutter/material.dart';
-import 'features/authentication/screens/signup_screen.dart';
 import 'features/splash/splash_screen.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
 void main() {
   runApp(SplashScreen(key:UniqueKey(),onInitializationComplete: ()=>runApp(MyApp())));
 }
@@ -12,22 +13,25 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: SignupScreen(),
+    return RepositoryProvider(
+        create: (context) => AuthService(),
+    child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        home: SigninScreen()
+    ),
     );
   }
 }
