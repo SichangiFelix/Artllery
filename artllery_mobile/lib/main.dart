0392820352ddded753c3d82735f7e4a21d0800c6
SichangiFelix <sichangifelix23@gmail.com>
2023-04-05 01:05:11 +0300

    Brought the BlocProvider further up the tree

--- artllery_mobile/lib/main.dart
+++ artllery_mobile/lib/main.dart
@@ -1,21 +1,39 @@
+import 'package:artllery_mobile/features/authentication/bloc/login_bloc.dart';
+import 'package:artllery_mobile/features/authentication/bloc/login_event.dart';
 import 'package:artllery_mobile/features/authentication/screens/signin_screen.dart';
 import 'package:artllery_mobile/features/authentication/services/auth_service.dart';
+import 'package:artllery_mobile/features/home/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'features/authentication/bloc/login_state.dart';
 import 'features/splash/splash_screen.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:http/http.dart' as http;
 void main() {
   runApp(SplashScreen(key:UniqueKey(),onInitializationComplete: ()=>runApp(MyApp())));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+
+
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  http.Client client = http.Client();
+
   @override
   Widget build(BuildContext context) {
     return RepositoryProvider(
-        create: (context) => AuthService(),
-    child: MaterialApp(
+        create: (context) => AuthService(client:  client),
+    child: BlocProvider<LoginBloc>(
+    create: (context) => LoginBloc(
+    authService: context.read<AuthService>(),
+    ),
+      child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(
@@ -30,7 +48,13 @@ class MyApp extends StatelessWidget {
           // is not restarted.
           primarySwatch: Colors.blue,
         ),
-        home: SigninScreen()
+        home: BlocBuilder<LoginBloc, LoginState>(
+          builder: (context, state){
+            context.read<LoginBloc>().add(CheckToken());
+            return state is LoggedIn ? const HomeScreen() : const SigninScreen();
+          },
+        ),
+      ),
     ),
     );
   }
