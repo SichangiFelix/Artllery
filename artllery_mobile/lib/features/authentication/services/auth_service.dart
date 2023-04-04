c5ef0ff2a25725227614a6f823ea4801372dd373
SichangiFelix <sichangifelix23@gmail.com>
2023-04-05 01:03:52 +0300

    Handled AuthService dependency injection Gave exceptions a more meaningful message Implemented logout

--- artllery_mobile/lib/features/authentication/services/auth_service.dart
+++ artllery_mobile/lib/features/authentication/services/auth_service.dart
@@ -1,13 +1,20 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:io';
 
 class AuthService{
 
-  static const String baseURL= "http://192.168.56.1:8000";
+  AuthService({
+    required http.Client client,
+    this.baseURL = "http://192.168.56.1:8000"
+}): _client = client;
+
+  final String baseURL;
+  final http.Client _client;
   String? accessToken;
 
- Future<String> loginUser({required String username, required String password}) async{
+  Future<String> loginUser({required String username, required String password}) async{
 
    final _url = Uri.parse('$baseURL/api/token/');
 
@@ -16,7 +23,7 @@ class AuthService{
      'password': password
    };
 
-   var response = await http.post(_url,
+   var response = await _client.post(_url,
      headers: <String, String>{
        'Content-Type': 'application/json',
      },
@@ -29,7 +36,7 @@ class AuthService{
      prefs.setString("refreshToken", jsonDecode(response.body)["refresh"]);
      return "User successfully logged in";
    } else {
-     throw Exception(response.body);
+     throw Exception('Status Code: ${response.statusCode},  body: ${response.body}',);
    }
  }
 
@@ -37,7 +44,7 @@ class AuthService{
 
     final _url = Uri.parse('$baseURL/register/');
 
-    var response = await http.post(_url,
+    var response = await _client.post(_url,
       headers: {
         'Content-Type': 'application/json',
       },
@@ -47,19 +54,18 @@ class AuthService{
     if (response.statusCode == 200) {
       return "User successfully created";
     } else {
-      throw Exception("Could not create user");
+      throw Exception('Status Code: ${response.statusCode},  body: ${response.body}',);
     }
   }
 
   Future<bool> isLoggedIn() async{
-
     final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString("accessToken") != null;
+  }
 
-   if(prefs.getString("accessToken") != null){
-     return true;
-   }else{
-     return false;
-   }
+  Future logout() async{
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.remove("accessToken");
   }
 
 }
