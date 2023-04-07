1e0552e45b5ebb382b609a3992123998c7b05a1f
SichangiFelix <sichangifelix23@gmail.com>
2023-04-07 16:42:38 +0300

    Completed auth setup

--- artllery_backend/artllery/artllery/urls.py
+++ artllery_backend/artllery/artllery/urls.py
@@ -19,7 +19,7 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from artlleryauth.views import UserViewSet
+from artlleryauth.views import UserViewSet, register
 from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
@@ -27,6 +27,7 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("register/", register, name="register")
 ]
 
 router = DefaultRouter()
--- artllery_backend/artllery/artlleryauth/views.py
+++ artllery_backend/artllery/artlleryauth/views.py
@@ -1,7 +1,13 @@
 from .serializers import UserSerializer
 from django.contrib.auth.models import User
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from django.views.decorators.csrf import csrf_exempt
+import jwt
+from rest_framework import status
+from rest_framework.response import Response
+from django.conf import settings
 
 
 # Create your views here.
@@ -10,3 +16,23 @@ class UserViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated,)
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+@api_view(['POST'])
+@csrf_exempt
+@permission_classes((AllowAny,))
+def register(request):
+    # Extract the user credentials from the request
+    first_name = request.data.get('first_name')
+    username = request.data.get('username')
+    email = request.data.get('email')
+    password = request.data.get('password')
+    
+    # Create a new user with the provided credentials
+    user = User.objects.create_user(username=username, email=email, password=password, first_name = first_name)
+    
+    # Generate a JWT token for the new user
+    payload = {'user_id': user.id}
+    jwt_token = jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
+    
+    # Return the JWT token in the response
+    return Response({'token': jwt_token}, status=status.HTTP_200_OK)
