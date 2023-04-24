a14c229f5a740d629e95369ce77deff07c038a86
SichangiFelix <sichangifelix23@gmail.com>
2023-04-24 13:09:14 +0300

    Built the artist screen: grid, circle avatar

---
+++ artllery_mobile/lib/features/artist/screens/artist_screen.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+class ArtistScreen extends StatefulWidget {
+  const ArtistScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ArtistScreen> createState() => _ArtistScreenState();
+}
+
+class _ArtistScreenState extends State<ArtistScreen> {
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(height: screenHeight/80,),
+            CircleAvatar(
+              radius: screenHeight / 14,
+            ),
+            SizedBox(height: screenHeight/50,),
+            const Text("Artist Name",
+              style: TextStyle(
+                fontSize: 26,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: screenWidth/30),
+              child: const Divider(
+                thickness: 2,
+              ),
+            ),
+            SizedBox(height: screenHeight/60,),
+            Expanded(
+              child: GridView.builder(
+                shrinkWrap: true,
+                padding: EdgeInsets.symmetric(horizontal: screenWidth/30, vertical: screenHeight/50),
+                gridDelegate: SliverWovenGridDelegate.count(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 4,
+                  crossAxisSpacing: 4,
+                  pattern: [
+                    const WovenGridTile(
+                        0.95,
+                        crossAxisRatio: 0.95,
+                    ),
+                    const WovenGridTile(
+                      0.7,
+                      crossAxisRatio: 0.9,
+                      alignment: AlignmentDirectional.centerEnd,
+                    ),
+                  ],
+                ),
+                itemCount: 8,
+                itemBuilder: (context, index) => Container(
+                  decoration: BoxDecoration(
+                    color: Colors.indigo,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
