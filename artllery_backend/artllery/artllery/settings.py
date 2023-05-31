9324c980246c7cdc72ff51eca7e302a9b5a32bc4
SichangiFelix <sichangifelix23@gmail.com>
2023-06-01 00:31:03 +0300

    Added configurations for media folder

--- artllery_backend/artllery/artllery/settings.py
+++ artllery_backend/artllery/artllery/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'artlleryauth',
     'rest_framework',
     'rest_framework.authtoken',
+    'gallery',
 ]
 
 REST_FRAMEWORK = {
@@ -147,3 +148,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_URL = '/media/'
