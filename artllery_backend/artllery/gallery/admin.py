80045463a4935b7d41d54d1ec0cded98ebf9223b
SichangiFelix <sichangifelix23@gmail.com>
2023-06-02 17:23:15 +0300

    de registered error causing model

--- artllery_backend/artllery/gallery/admin.py
+++ artllery_backend/artllery/gallery/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Art, ArtImage, Artist
+from .models import Art, ArtImage
 
 admin.site.register(Art)
 admin.site.register(ArtImage)
-admin.site.register(Artist)
+# admin.site.register(Artist)
