125d122c2e13ac65e5d07174fd4523fbc69b52fa
SichangiFelix <sichangifelix23@gmail.com>
2023-06-02 17:24:10 +0300

    Updated the foreign key

--- artllery_backend/artllery/gallery/models.py
+++ artllery_backend/artllery/gallery/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Art(models.Model):
@@ -9,17 +10,18 @@ class Art(models.Model):
     datePosted = models.DateTimeField(auto_now_add=True)
     materials = models.CharField(max_length=200)
     status = models.CharField(max_length=20)
-    artist = models.ForeignKey(on_delete=models.CASCADE) #add User model referenced
+    artist = models.ForeignKey(User,on_delete=models.CASCADE) #add User model referenced
 
 class ArtImage(models.Model):
     image = models.ImageField(upload_to="images/")
     art = models.ForeignKey(Art, on_delete=models.CASCADE)
 
-class Artist(models.Model):
-    first_name = models.CharField(max_length=30)
-    second_name = models.CharField(max_length=30)
-    username = models.CharField(max_length=30)
-    email = models.CharField(max_length=20)
-    phone = models.CharField(max_length=20)
+# class Artist(models.Model):
+#     first_name = models.CharField(max_length=30)
+#     second_name = models.CharField(max_length=30)
+#     username = models.CharField(max_length=30)
+#     email = models.CharField(max_length=20)
+#     phone = models.CharField(max_length=20)
+#Use this custom Artist model to add extra user details
 
 #Research on how to rep cart
