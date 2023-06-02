5f8623ca8481114d84ff6dc4271e190d0b585dd8
SichangiFelix <sichangifelix23@gmail.com>
2023-06-02 17:24:33 +0300

    Built Art and ArtImage views

--- artllery_backend/artllery/gallery/views.py
+++ artllery_backend/artllery/gallery/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import *
+from .serializers import *
+
 
 # Create your views here.
+class ListCreateArt(generics.ListCreateAPIView):
+    queryset = Art.objects.all()
+    serializer_class = ArtSerializer
+    authentication_classes = []
+class RetrieveUpdateDestroyArt(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Art.objects.all()
+    serializer_class = ArtSerializer
+class ListCreateArtImage(generics.ListCreateAPIView):
+    queryset = ArtImage.objects.all()
+    serializer_class = ArtImageSerializer
+class RetrieveUpdateDestroyArtImage(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ArtImage.objects.all()
+    serializer_class = ArtImageSerializer
