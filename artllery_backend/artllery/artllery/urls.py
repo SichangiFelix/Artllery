df8cb9729e01ff528493f0477adda831dfbac9e9
SichangiFelix <sichangifelix23@gmail.com>
2023-06-02 17:22:50 +0300

    Added gallery urls

--- artllery_backend/artllery/artllery/urls.py
+++ artllery_backend/artllery/artllery/urls.py
@@ -14,7 +14,10 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path("register/", register, name="register")
+    path("register/", register, name="register"),
+
+    # Gallery section
+    path('gallery/', include('gallery.urls')),
 ]
 
 router = DefaultRouter()
