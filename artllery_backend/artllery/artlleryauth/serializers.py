c606383fae5fb006eef09949f7f9e85e2ba28cec
SichangiFelix <sichangifelix23@gmail.com>
2023-10-02 23:56:55 +0300

    Added field from both default user model and Profile model to the serializer

--- artllery_backend/artllery/artlleryauth/serializers.py
+++ artllery_backend/artllery/artlleryauth/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from .models import Profile
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = "__all__"
+        model = Profile
+        fields = (
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'address',
+            'profile_picture',
+            'phone'
+        )
